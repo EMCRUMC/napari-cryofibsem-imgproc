6a985da633f854cfd349c05c74cc8f5d7685140e
Shaina To <165912537+shainato@users.noreply.github.com>
2024-09-19 17:36:12 +0200

    Update __init__.py

--- src/napari_cryofibsem_imgproc/__init__.py
+++ src/napari_cryofibsem_imgproc/__init__.py
@@ -1,6 +1,11 @@
 __version__ = "0.0.1"
 from ._sample_data import make_sample_data
 from ._widget import ExampleQWidget, ImageThreshold, threshold_autogenerate_widget, threshold_magic_widget
+from .brightness import brightness
+from .contrast import contrast
+from .decharge import decharge
+from .decurtain import decurtain
+from .denoise import denoise
 
 __all__ = (
     "make_sample_data",
@@ -8,4 +13,9 @@ __all__ = (
     "ImageThreshold",
     "threshold_autogenerate_widget",
     "threshold_magic_widget",
+    "brightness"
+    "contrast",
+    "decharge",
+    "decurtain",
+    "denoise",
 )
