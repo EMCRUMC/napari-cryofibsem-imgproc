833582f8045f9c5aace711bd2028d7b6a4e9e70e
Shaina To <165912537+shainato@users.noreply.github.com>
2025-07-14 11:52:14 +0200

    Update __init__.py

--- src/napari_cryofibsem_imgproc/__init__.py
+++ src/napari_cryofibsem_imgproc/__init__.py
@@ -6,6 +6,7 @@ from .contrast import contrast
 from .decharge import decharge
 from .decurtain import decurtain
 from .denoise import denoise
+from .medianblur import medianblur
 
 __all__ = (
     "make_sample_data",
@@ -18,4 +19,5 @@ __all__ = (
     "decharge",
     "decurtain",
     "denoise",
+    "medianblur"
 )
